c9057d144589c84a4dd3d433f5254b9a169a2d3c
Colleen Geohagan <ckbowker@gmail.com>
2016-07-20 21:47:00 -0400

    Adding mobile layout, styling changes, and JS for message area

--- js/app.js
+++ js/app.js
@@ -1,4 +1,47 @@
-// When click on alert, make it disappear
+// Alert Button 
+// Make button disappear when the button is clicked
 $('.alert').click(function(){
 	$('.alert').hide();
 });
+
+//Message User Widget functionality
+
+var $submit = $('.submit_alert');
+var $userError = $('.user_error');
+var $messageError = $('.message_error');
+var $userSearch = $('#user_search');
+var $userMessage = $('#user_message');
+
+//Hide the alert messages
+$submit.hide();
+$userError.hide();
+$messageError.hide();
+
+// 1. Use JS to allow you to submit form and display a confirmation the message was sent.
+$('button#send').click(function(){
+	event.preventDefault();
+	// 2. Use JS to display error messages if user isn't selected or message field is empty.
+	if($userSearch.val() == '') {
+		$userError.show();
+	} 
+	if ($userMessage.val() == '') {
+		$messageError.show();
+	}
+	if(!$userSearch.val() == '' && !$userMessage.val() == '') {
+		$submit.show();
+	}
+});
+
+$submit.click(function(){
+	$submit.hide();
+});
+
+$userSearch.keyup(function(){
+	$userError.hide();
+});
+
+$userMessage.keyup(function(){
+	$messageError.hide();
+});
+
+
